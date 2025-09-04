b6d1d5dc1502d74b8c796d196ad9bf18a4087707
Carlos Tellini <74674589+CTellini@users.noreply.github.com>
2025-09-03 21:27:02 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
+//server.hmr.overlay : false
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
